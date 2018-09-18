d8a53ea0479693e3608ee3cb148b62ff213b855a
duy <duy123galaxy13@gmail.com>
2018-09-19 00:11:14 +0700

    homework
    finish the homework

--- src/week1/Student.java
+++ src/week1/Student.java
@@ -3,14 +3,50 @@ package week1;
 public class Student {
 
     // TODO: khai báo các thuộc tính cho Student
-
+    private String name;
+    private String id;
+    private String group;
+    private String email;
     // TODO: khai báo các phương thức getter, setter cho Student
+
+    public void setName(String n) {
+        name = n;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setId(String i) {
+        id = i;
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setGroup(String g) {
+        group = g;
+    }
+    public String getGroup() {
+        return group;
+    }
+
+    public void setEmail(String e) {
+        email = e;
+    }
+    public String getEmail() {
+        return email;
+    }
+
 
     /**
      * Constructor 1
      */
     Student() {
         // TODO:
+        name = "Student";
+        id = "000";
+        group = "INT22041";
+        email = "uet@vnu.edu.vn";
     }
 
     /**
@@ -21,6 +57,10 @@ public class Student {
      */
     Student(String n, String sid, String em) {
         // TODO:
+        name = n;
+        id = sid;
+        email = em;
+        group = "INT22041";
     }
 
     /**
@@ -29,10 +69,18 @@ public class Student {
      */
     Student(Student s) {
         // TODO:
+        this.group = s.getGroup();
+        this.email = s.getEmail();
+        this.id = s.getId();
+        this.name = s.getName();
     }
 
     String getInfo() {
         // TODO:
-        return null; // xóa dòng này sau khi cài đặt
+        System.out.println(name);
+        System.out.println(id);
+        System.out.println(group);
+        System.out.println(email);
+        return null;
     }
 }
--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -3,21 +3,102 @@ package week1;
 public class StudentManagement {
 
     // TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
+    public static int amountStudents = 100;
+    public static Student[] students = new Student[amountStudents];
 
-    public boolean sameGroup(Student s1, Student s2) {
+    public static boolean sameGroup(Student s1, Student s2) {
         // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+        return s1.getGroup() == s2.getGroup();
     }
 
-    void studentsByGroup() {
+    static void studentsByGroup() {
         // TODO:
+        for (int i = 0; i < 100; i++) {
+            Student currentStudent = students[i];
+            if( currentStudent.getGroup() == "INT22042") {
+                currentStudent.getInfo();
+                System.out.println("\n");
+            }
+        }
     }
 
     void removeStudent(String id) {
         // TODO:
+        for(int i = 0; i < 100; i++) {
+            if ( students[i].getId() == id ) {
+                amountStudents -= 1;
+                for(int j = 0;j < amountStudents; j++) {
+                    students[j] = students[j+1];
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
         // TODO:
+
+        //Student studen_A = new Student();
+        // a: Tao ra mot sinh vien.
+        Student newStudent = new Student();
+
+        // b : thiet lap cac thong tin ve sinh vien.
+        newStudent.setEmail("lecanhduy@gmail.com");
+        newStudent.setGroup("INT2204_4");
+        newStudent.setId("17020647");
+        newStudent.setName("LeCanhDuy");
+
+        // c: in ra ten cua sinh vien.
+        System.out.println(newStudent.getName());
+
+        // d: in ra toan bo thong tin cua sinh vien.
+        newStudent.getInfo();
+
+        // Constructor 1
+        Student checkConstructor1 = new Student();
+        checkConstructor1.getInfo();
+
+        // Constructor 2
+        Student checkConstructor2 = new Student("le canh duy","17020647","lecanhduylsdd@gmail.com");
+        checkConstructor2.getInfo();
+
+        // Constructor 3
+        Student checkConstructor3 = new Student(checkConstructor2);
+        System.out.println("This is Constructor3 test:");
+        checkConstructor3.getInfo();
+
+        // Test sameGroup method
+        Student a = new Student();
+        a.setGroup("INT22041");
+
+        Student b = new Student();
+        b.setGroup("INT22041");
+
+        Student c= new Student();
+        c.setGroup("INT22042");
+
+        System.out.print(sameGroup(a,b));
+        System.out.println("\n");
+        System.out.print(sameGroup(a,c));
+        System.out.println("\n");
+
+
+        // test method StudentsByGroup
+        for(int i = 0; i < 100; i++) {
+            students[i] = new Student();
+        }
+
+        students[0].setGroup("INT22042");
+        students[0].setName("hoc sinh 0");
+
+        students[1].setGroup("INT22042");
+        students[1].setName("hoc sinh 1");
+
+        students[2].setGroup("INT22041");
+        students[2].setName("hoc sinh 2");
+
+        students[3].setGroup("INT22042");
+        students[3].setName("hoc sinh 3");
+
+        studentsByGroup();
     }
 }

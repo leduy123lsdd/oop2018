07e7c4711c3539642f03988efdcb16cbcd9ac739
duy <duy123galaxy13@gmail.com>
2018-10-18 23:17:52 +0700

    xog

---
+++ src/week5_6/Rectangle.java
@@ -0,0 +1,22 @@
+package week5_6;
+
+public class Rectangle {
+    private int width = 0;
+    private int height = 0;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+}
---
+++ src/week5_6/Square.java
@@ -0,0 +1,31 @@
+package week5_6;
+
+public class Square {
+    private int x = 0;
+    private int y = 0;
+    private int size = 0;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
